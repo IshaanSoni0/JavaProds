f5225714cc29c18387aff73912978213ef4dcb57
Ishaan <ishaansoni.1535@gmail.com>
2025-05-18 22:06:17 -0700

    inital test changes to App, and changes to gitignore for /bin/, /.vscode/, and /lib/

--- cleanproj/src/App.java
+++ cleanproj/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Commit test");
+
     }
 }
+//  use: javac -d bin src/App.java -------- to compile to bin
+//  use: java -cp bin App -------- to run the App.class in bin 
