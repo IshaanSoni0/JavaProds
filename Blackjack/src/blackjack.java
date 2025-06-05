79febaf81bc646129a609144063b8325a44f4dc1
Ishaan <ishaansoni.1535@gmail.com>
2025-06-05 14:42:31 -0700

    made blackjack prodj

---
+++ Blackjack/src/blackjack.java
@@ -0,0 +1,9 @@
+public class blackjack {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Commit test");
+
+    }
+}
+//  use: javac -d bin src/blackjack.java -------- to compile to bin
+//  use: java -cp bin blackjack -------- to run the blackjack.class in bin 
