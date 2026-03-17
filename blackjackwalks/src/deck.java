290dceabc6d2ef0fe58650ec1b44346ede47aa3f
Ishaan <ishaansoni.1535@gmail.com>
2026-03-17 01:18:20 -0700

    tf

--- blackjackwalks/src/deck.java
+++ blackjackwalks/src/deck.java
@@ -15,6 +15,16 @@ public class deck {
         return deck.size();
     }
     public card getRandCard(){
+        if (deck.size() == 0) {
+            // Reinitialize deck if empty to avoid IndexOutOfBoundsException
+            deck = new ArrayList<card>();
+            for(int s = 0; s<4; s++){
+                for(int v = 1; v<14;v++){
+                    card card = new card(s,v);
+                    deck.add(card);
+                }
+            }
+        }
         int rand = (int)(Math.random() * deck.size());
         card c = deck.get(rand);
         deck.remove(rand);
